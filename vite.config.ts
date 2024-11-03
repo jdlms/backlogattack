1010f069712a6bc5633d838f4272eaaf78bd1c48
jdlms <lamorey.joshua@gmail.com>
2024-11-03 20:46:39 +0100

    import fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './app')
+    }
+  },
   plugins: [
     remix({
       future: {
@@ -14,3 +20,4 @@ export default defineConfig({
     tsconfigPaths(),
   ],
 });
+
