88d8ab4c9176e4c419ad3952a0d41c26776c4152
jdlms <lamorey.joshua@gmail.com>
2024-11-03 20:47:09 +0100

    remix default landing cleanup

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,25 +3,22 @@ import type { MetaFunction } from "@remix-run/node";
 export const meta: MetaFunction = () => {
   return [
     { title: "Backlog Attack" },
-    { name: "description", content: "Game price alerts" },
+    { name: "description", content: "Need More Games!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex h-screen items-start pt-24   justify-center">
       <div className="flex flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Welcome to <span className="sr-only">BACKLOG ATTACK</span>
+          <h1 className="leading text-2xl font-bold text-gray-100">
+            Welcome to
+            <br />
+            <span className="text-base md:text-4xl">BACKLOG ATTACK</span>
           </h1>
           <div className="h-[144px] w-[434px]"></div>
         </header>
-        <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-          <p className="leading-6 text-gray-700 dark:text-gray-200">
-            What&apos;s next?
-          </p>
-        </nav>
       </div>
     </div>
   );
